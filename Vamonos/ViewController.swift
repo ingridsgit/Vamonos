778e7d625a8ca3306bb295df5316464f8b2a7bc4
Ingrid Bazin <ingrid.bazin31@gmail.com>
2019-06-07 13:43:20 +0200

    Initial Commit

---
+++ Vamonos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vamonos
+//
+//  Created by Admin on 07/06/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

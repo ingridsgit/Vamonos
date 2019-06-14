496ade2cda8fdc562fc9dfb16a28e4257c884c6b
Ingrid Bazin <ingrid.bazin31@gmail.com>
2019-06-14 17:04:34 +0200

    Add coordinates

--- Vamonos/AirportViewController.swift
+++ Vamonos/AirportViewController.swift
@@ -17,23 +17,58 @@ class AirportViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setMapToUserLocation()
+        
+}
+    
+    func setMapToUserLocation(){
+        let parameters: Parameters = ["locale": "fr", "callback": "useriata"]
+        let headers : HTTPHeaders = ["x-access-token": APIRequests.API_KEY]
+        let myRequest = Alamofire.request("https://www.travelpayouts.com/whereami?locale=fr&callback=", parameters: parameters, headers: headers)
+        myRequest.responseJSON(completionHandler: { (dataResponse) in
+            if dataResponse.result.isSuccess {
+                print(dataResponse.result.value)
+                if let rootArray = dataResponse.result.value as? [String:Any] {
+                    if let coordinateString = rootArray["coordinates"] as? String {
+                        let coordinatesSplit = coordinateString.split(separator: ":")
+                        if let latitude = Double(coordinatesSplit[0]),
+                            let longitude = Double(coordinatesSplit[1]){
+                        let coordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                            self.mapView.centerCoordinate = coordinates
+                        }
+                    }
+                    ajouter weak self
+                }
+            }
+            else {
+                print(dataResponse.result.debugDescription)
+            }
+        })
     }
     
     @IBAction func onSearchClicked(_ sender: Any) {
         if let input = searchField.text {
             let parameters: Parameters = ["term": input]
-            let myRequest = Alamofire.request("http://nano.aviasales.ru/places_fr", parameters: parameters)
+            let headers : HTTPHeaders = ["x-access-token": APIRequests.API_KEY]
+            let myRequest = Alamofire.request("https://nano.aviasales.ru/places_en", parameters: parameters, headers: headers)
             myRequest.responseJSON(completionHandler: { (dataResponse) in
                 if dataResponse.result.isSuccess {
-                    if let rootArray = dataResponse.result.value as? [Any] {
-//                        print(rootArray[0].description)
+                    if let rootArray = dataResponse.result.value as? [[String:Any]] {
+                        for array in rootArray {
+//                            if let title = array["title"] as? String {
+//                                print(title)
+//                            }
+                            print(array.description)
+                        }
                     }
                 }
+                else {
+                    print(dataResponse.result.debugDescription)
+                }
             })
-            
-            
-            
         }
     }
     
---
+++ Vamonos/model/APIRequests.swift
@@ -0,0 +1,19 @@
+//
+//  APIRequests.swift
+//  Vamonos
+//
+//  Created by Admin on 14/06/2019.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class APIRequests {
+    static let API_KEY = "1d82d8fede2d239756a91ef78bfb601a"
+    
+    static func getUserLocation(){
+        
+    }
+    
+}
